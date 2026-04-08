d9b57dac09b0f4271fa36788b543327481301e51
optoolfactory <80941254+optoolfactory@users.noreply.github.com>
2026-04-08 17:08:52 +0900

    Update process_config.py

--- system/manager/process_config.py
+++ system/manager/process_config.py
@@ -83,7 +83,7 @@ def run_speed_limit_vision(started: bool, params: Params, CP: car.CarParams, sta
 procs = [
   DaemonProcess("manage_athenad", "system.athena.manage_athenad", "AthenadPid"),
 
-  NativeProcess("loggerd", "system/loggerd", ["./loggerd"], and_(allow_logging, logging)),
+  #NativeProcess("loggerd", "system/loggerd", ["./loggerd"], and_(allow_logging, logging)),
   NativeProcess("encoderd", "system/loggerd", ["./encoderd"], and_(allow_logging, only_onroad)),
   NativeProcess("stream_encoderd", "system/loggerd", ["./encoderd", "--stream"], notcar),
   PythonProcess("logmessaged", "system.logmessaged", always_run),
