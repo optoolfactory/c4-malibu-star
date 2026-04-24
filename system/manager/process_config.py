8dae69f0d0cd35e17ab9726164953af54d2ff00a
optoolfactory <80941254+optoolfactory@users.noreply.github.com>
2026-04-24 22:22:02 +0900

    Update process_config.py

--- system/manager/process_config.py
+++ system/manager/process_config.py
@@ -76,7 +76,7 @@ def run_speed_limit_filler(started: bool, params: Params, CP: car.CarParams, fro
 procs = [
   DaemonProcess("manage_athenad", "system.athena.manage_athenad", "AthenadPid"),
 
-  NativeProcess("loggerd", "system/loggerd", ["./loggerd"], and_(allow_logging, logging)),
+  #NativeProcess("loggerd", "system/loggerd", ["./loggerd"], and_(allow_logging, logging)),
   NativeProcess("encoderd", "system/loggerd", ["./encoderd"], and_(allow_logging, only_onroad)),
   NativeProcess("stream_encoderd", "system/loggerd", ["./encoderd", "--stream"], notcar),
   PythonProcess("logmessaged", "system.logmessaged", always_run),
